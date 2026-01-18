a172cc28b6f4ae373508f3aaeb2964bef23141ab
muzaffarvv <muzaffarxomitaliyev25@gmail.com>
2026-01-18 15:50:50 +0500

    fix config

--- user-service/src/main/kotlin/uz/vv/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/uz/vv/userservice/UserServiceApplication.kt
@@ -2,8 +2,14 @@ package uz.vv.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import uz.vv.userservice.base.BaseRepoImpl
 
 @SpringBootApplication
+@EnableJpaRepositories(
+	basePackages = ["uz.vv.userservice.repo"],
+	repositoryBaseClass = BaseRepoImpl::class
+)
 class UserServiceApplication
 
 fun main(args: Array<String>) {
